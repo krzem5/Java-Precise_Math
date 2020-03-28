2093b0fdc4951f9a8ecd9afae3cc3afd00999821
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 13:48:54 +0100

    Push Update

---
+++ com/krzem/precise_math/Main.java
@@ -0,0 +1,15 @@
+package com.krzem.precise_math;
+
+
+
+public class Main{
+	public static void main(String[] args){
+		new Main();
+	}
+
+
+
+	public Main(){
+		System.out.println(new PreciseNumber("59").sub(new PreciseNumber("1")));
+	}
+}
