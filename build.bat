588f729c63e7ea3fda108981bf291258c004808c
Krzem <krzem5.dev@gmail.com>
2021-04-10 10:41:18 +0200

    Push Update 04/10/2021, 10:41:00

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/precise_math/Main.java&&jar cvmf ../manifest.mf ../build/precise_math.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="precise_math.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/precise_math.jar
-:end
+python build.py %*
